755f77a4134a09872fe4db019524d1f73b309a7d
srao <srao@accuratebackground.com>
2020-04-13 14:09:51 +0530

    day3 added service layer and junits advance cancepts

---
+++ day3/orderservice/src/main/java/com/order/orderservice/service/CustomerDao.java
@@ -0,0 +1,17 @@
+package com.order.orderservice.service;
+
+
+public class CustomerDao {
+
+	public Customer save(Customer customer) {
+		return customer;
+
+		//TODO
+	}
+
+	public void updateEmail(Customer customer, String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
