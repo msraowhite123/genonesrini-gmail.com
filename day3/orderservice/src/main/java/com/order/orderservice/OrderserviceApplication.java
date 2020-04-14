7d1e31b178b9dcab0695f05e19affcaeac7bf7be
srao <srao@accuratebackground.com>
2020-04-14 10:30:55 +0530

    day 3 hal browser added

--- day3/orderservice/src/main/java/com/order/orderservice/OrderserviceApplication.java
+++ day3/orderservice/src/main/java/com/order/orderservice/OrderserviceApplication.java
@@ -1,14 +1,30 @@
 package com.order.orderservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.order.orderservice.controller.CustomerRepository;
+import com.order.orderservice.model.Customer;
+
+
 @SpringBootApplication
-public class OrderserviceApplication {
+public class OrderserviceApplication implements CommandLineRunner{
 
-
+	@Autowired
+	CustomerRepository customerRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(OrderserviceApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		customerRepository.save(new Customer("Jack", "Smith"));
+		customerRepository.save(new Customer("Adam", "Johnson"));
+		customerRepository.save(new Customer("Kim", "Smith"));
+		customerRepository.save(new Customer("David", "Williams"));
+		customerRepository.save(new Customer("Peter", "Davis"));
 	}
 
 }
